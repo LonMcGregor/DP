e8609709752d378ad8ea8fd0b43ebddad9ec605d
Léon McGregor <lonmcgregor@outlook.com>
2015-10-26 14:35:33 +0000

    A: Initial Commit for I229 | Reverse FizzBuzz
    This was sitting in my repo. I must have started but never completed it

---
+++ dp/imd/revfizzbuzz/ReverseFizzBuzz.java
@@ -0,0 +1,16 @@
+/* /r/DailyProgrammer I229 */
+
+import java.util.ArrayList;
+class ReverseFizzBuzz {
+	public static void main(String[] args){
+		ArrayList<String> entries = new ArrayList<String>();
+		String input = System.in.readLine();
+		while(!input.equals("\n")){
+			entries.add(input);
+		}
+		/*
+			For each entry in the lines
+			if there is a new letter
+		*/
+	}
+}
